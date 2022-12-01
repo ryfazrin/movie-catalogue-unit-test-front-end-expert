f7a7fc838fa9dc13db7b362a7bf023b0d1bc3ae6
riansera515@gmail.com <>
2022-12-01 08:51:27 +0700

    added: new test save movie

--- specs/LikeMovieSpec.js
+++ specs/LikeMovieSpec.js
@@ -1,3 +1,4 @@
+import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
 import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
 
 describe('Liking A Movie', () => {
@@ -13,8 +14,34 @@ describe('Liking A Movie', () => {
 
     expect(document.querySelector('[aria-label="like this movie"]'))
       .toBeTruthy();
+  });
+
+  it('should not show the like button when the movie has not been liked before', async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
 
     expect(document.querySelector('[aria-label="unlike this movie"]'))
       .toBeFalsy();
+  });
+
+  it('should be able to like te movie', async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+    const movie = await FavoriteMovieIdb.getMovie(1);
+    expect(movie).toEqual({ id: 1 });
   });
 });
