223c8b4daf991ee547d495820c8463803bc1ba5e
riansera515@gmail.com <>
2022-12-01 12:47:07 +0700

    added: test in unlike movie

--- specs/unlikeMovieSpec.js
+++ specs/unlikeMovieSpec.js
@@ -38,4 +38,31 @@ describe('Unliking A Movie', () => {
     expect(document.querySelector('[aria-label="like this movie"]'))
       .toBeFalsy();
   });
+
+  it('should be able to remove liked movie from the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('[aria-label="unlike this movie"]').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteMovieIdb.getAllMovies()).toEqual([]);
+  });
+
+  it('should not throw error if the unliked movie is not in the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+    // hapus dulu film dari daftar film yang disukai
+    await FavoriteMovieIdb.deleteMovie(1);
+    // kemudian, simulasikan pengguna menekan widget batal menyukai film
+    document.querySelector('[aria-label="unlike this movie"]').dispatchEvent(new Event('click'));
+    expect(await FavoriteMovieIdb.getAllMovies()).toEqual([]);
+  });
 });
