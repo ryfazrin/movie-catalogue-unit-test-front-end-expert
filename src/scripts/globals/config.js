2e1746cdb1ecd696f60071a74d93a05e25529173
riansera515@gmail.com <>
2022-12-02 19:21:40 +0700

    updated: fix page detail with add favoriteMovies

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '0df851cf67581bdb614ee182cb1c87de',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
