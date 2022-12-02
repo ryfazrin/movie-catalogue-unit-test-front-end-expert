d7ec7c40a7491a3e0c23f39f27e6cffa3b5a0e92
riansera515@gmail.com <>
2022-12-02 21:47:13 +0700

    fix: duplicate code

--- specs/favoriteMovieSearchSpec.js
+++ specs/favoriteMovieSearchSpec.js
@@ -1,7 +1,9 @@
-import FavoriteMovieSearchPresenter from "../src/scripts/views/pages/liked-movies/favorite-movie-search-presenter";
+import FavoriteMovieSearchPresenter from '../src/scripts/views/pages/liked-movies/favorite-movie-search-presenter';
 import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
 
 describe('Search movies', () => {
+  let presenter;
+
   beforeEach(() => {
     document.body.innerHTML = `
       <div id="movie-search-container">
@@ -11,12 +13,12 @@ describe('Search movies', () => {
         </div>
       </div>
     `;
-  });
-  
-  it('should be able to capture the query typed by the user', () => {
-    spyOn(FavoriteMovieIdb, 'searchMovies');
-    const presenter = new FavoriteMovieSearchPresenter({ favoriteMovies: FavoriteMovieIdb });
 
+    spyOn(FavoriteMovieIdb, 'searchMovies');
+    presenter = new FavoriteMovieSearchPresenter({ favoriteMovies: FavoriteMovieIdb });
+  });
+
+  it('should be able to capture the query typed by the user', () => {
     const queryElement = document.getElementById('query');
     queryElement.value = 'film a';
     queryElement.dispatchEvent(new Event('change'));
@@ -25,9 +27,6 @@ describe('Search movies', () => {
   });
 
   it('should ask the model to search for liked movies', () => {
-    spyOn(FavoriteMovieIdb, 'searchMovies');
-    const presenter = new FavoriteMovieSearchPresenter({ favoriteMovies: FavoriteMovieIdb });
-
     const queryElement = document.getElementById('query');
     queryElement.value = 'film a';
     queryElement.dispatchEvent(new Event('change'));
