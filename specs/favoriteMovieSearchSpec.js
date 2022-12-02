0b09d852747279bf2d1fe4ae917b48d7487953d6
riansera515@gmail.com <>
2022-12-02 19:39:47 +0700

    added: dummy template search

--- specs/favoriteMovieSearchSpec.js
+++ specs/favoriteMovieSearchSpec.js
@@ -1,3 +1,16 @@
 describe('Search movies', () => {
-  it('should be able to capture the query typed by the user', () => {});
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div id="movie-search-container">
+        <input type="text" id="query">
+        <div class="movie-result-container">
+          <ul class="movies"></ul>
+        </div>
+      </div>
+    `;
+  });
+  
+  it('should be able to capture the query typed by the user', () => {
+
+  });
 });
