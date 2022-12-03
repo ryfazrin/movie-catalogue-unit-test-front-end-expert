537cc3e2f03dd8761a9d06a5ecd11c5dbee08c0d
riansera515@gmail.com <>
2022-12-03 11:52:54 +0700

    updated: add describe in describe

--- specs/favoriteMovieSearchSpec.js
+++ specs/favoriteMovieSearchSpec.js
@@ -33,57 +33,59 @@ describe('Search movies', () => {
     constructPresenter();
   });
 
-  it('should be able to capture the query typed by the user', () => {
-    searchMovies('film a');
+  describe('When query is not empty', () => {
+    it('should be able to capture the query typed by the user', () => {
+      searchMovies('film a');
 
-    expect(presenter.latestQuery).toEqual('film a');
-  });
+      expect(presenter.latestQuery).toEqual('film a');
+    });
 
-  it('should ask the model to search for liked movies', () => {
-    searchMovies('film a');
+    it('should ask the model to search for liked movies', () => {
+      searchMovies('film a');
 
-    expect(FavoriteMovieIdb.searchMovies).toHaveBeenCalledWith('film a');
-  });
+      expect(FavoriteMovieIdb.searchMovies).toHaveBeenCalledWith('film a');
+    });
 
-  it('should show the title of the found movies', () => {
-    presenter._showFoundMovies([{ id: 1, title: 'Satu' }]);
-    expect(document.querySelectorAll('.movie__title').item(0).textContent)
-      .toEqual('Satu');
+    it('should show the title of the found movies', () => {
+      presenter._showFoundMovies([{ id: 1, title: 'Satu' }]);
+      expect(document.querySelectorAll('.movie__title').item(0).textContent)
+        .toEqual('Satu');
 
-    presenter._showFoundMovies([
-      { id: 1, title: 'Satu' },
-      { id: 2, title: 'Dua' },
-    ]);
+      presenter._showFoundMovies([
+        { id: 1, title: 'Satu' },
+        { id: 2, title: 'Dua' },
+      ]);
 
-    const movieTitles = document.querySelectorAll('.movie__title');
-    expect(movieTitles.item(0).textContent).toEqual('Satu');
-    expect(movieTitles.item(1).textContent).toEqual('Dua');
-  });
+      const movieTitles = document.querySelectorAll('.movie__title');
+      expect(movieTitles.item(0).textContent).toEqual('Satu');
+      expect(movieTitles.item(1).textContent).toEqual('Dua');
+    });
 
-  it('should show - for found movie without title', () => {
-    presenter._showFoundMovies([{ id: 1 }]);
+    it('should show - for found movie without title', () => {
+      presenter._showFoundMovies([{ id: 1 }]);
 
-    expect(document.querySelectorAll('.movie__title').item(0).textContent)
-      .toEqual('-');
-  });
+      expect(document.querySelectorAll('.movie__title').item(0).textContent)
+        .toEqual('-');
+    });
 
-  it('should show the movies found by Favorite Movies', (done) => {
-    document.getElementById('movie-search-container')
-      .addEventListener('movies:searched:updated', () => {
-        const movieTitles = document.querySelectorAll('.movie__title');
-        expect(movieTitles.item(0).textContent).toEqual('film abc');
-        expect(movieTitles.item(1).textContent).toEqual('ada juga film abcde');
-        expect(movieTitles.item(2).textContent).toEqual('ini juga boleh film a');
+    it('should show the movies found by Favorite Movies', (done) => {
+      document.getElementById('movie-search-container')
+        .addEventListener('movies:searched:updated', () => {
+          const movieTitles = document.querySelectorAll('.movie__title');
+          expect(movieTitles.item(0).textContent).toEqual('film abc');
+          expect(movieTitles.item(1).textContent).toEqual('ada juga film abcde');
+          expect(movieTitles.item(2).textContent).toEqual('ini juga boleh film a');
 
-        done();
-      });
+          done();
+        });
 
-    FavoriteMovieIdb.searchMovies.withArgs('film a').and.returnValues([
-      { id: 111, title: 'film abc' },
-      { id: 222, title: 'ada juga film abcde' },
-      { id: 333, title: 'ini juga boleh film a' },
-    ]);
+      FavoriteMovieIdb.searchMovies.withArgs('film a').and.returnValues([
+        { id: 111, title: 'film abc' },
+        { id: 222, title: 'ada juga film abcde' },
+        { id: 333, title: 'ini juga boleh film a' },
+      ]);
 
-    searchMovies('film a');
+      searchMovies('film a');
+    });
   });
 });
