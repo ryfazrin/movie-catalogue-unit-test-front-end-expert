efabd87ba99ec16b87653a5f8c8ea80d2aa26e3a
ryfazrin <riansera515@gmail.com>
2022-12-04 17:03:31 +0700

    remove: unnecessary tests

--- specs/favoriteMovieShowSpec.js
+++ specs/favoriteMovieShowSpec.js
@@ -15,21 +15,6 @@ describe('Showing all favorite movies', () => {
   });
 
   describe('When no movies have been liked', () => {
-    it('should render the information that no movies have been liked', () => {
-      const favoriteMovies = spyOnAllFunctions(FavoriteMovieIdb);
-
-      const presenter = new FavoriteMovieShowPresenter({
-        view,
-        favoriteMovies,
-      });
-
-      const movies = [];
-      presenter._displayMovies(movies);
-
-      expect(document.querySelectorAll('.movie-item__not__found').length)
-        .toEqual(1);
-    });
-
     it('should ask for the favorite movies', () => {
       const favoriteMovies = spyOnAllFunctions(FavoriteMovieIdb);
 
@@ -58,31 +43,6 @@ describe('Showing all favorite movies', () => {
   });
 
   describe('When favorite movies exist', () => {
-    it('should render the movies', () => {
-      const favoriteMovies = spyOnAllFunctions(FavoriteMovieIdb);
-      const presenter = new FavoriteMovieShowPresenter({
-        view,
-        favoriteMovies,
-      });
-
-      presenter._displayMovies([
-        {
-          id: 11,
-          title: 'A',
-          vote_average: 3,
-          overview: 'Sebuah film A',
-        },
-        {
-          id: 22,
-          title: 'B',
-          vote_average: 4,
-          overview: 'Sebuah film B',
-        },
-      ]);
-
-      expect(document.querySelectorAll('.movie-item').length).toEqual(2);
-    });
-
     it('should show the movies', (done) => {
       document.getElementById('movies').addEventListener('movies:updated', () => {
         expect(document.querySelectorAll('.movie-item').length).toEqual(2);
