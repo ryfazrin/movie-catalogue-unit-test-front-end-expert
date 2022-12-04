b89575783d0f1ac363e3532e3d37e7765d5eb7e1
ryfazrin <riansera515@gmail.com>
2022-12-04 19:31:54 +0700

    created: first end to end test scenario: showing empty liked movies

--- e2e/Liking_Movies.spec.js
+++ e2e/Liking_Movies.spec.js
@@ -1,5 +1,10 @@
 Feature('Liking Movies');
 
-Scenario('test something',  ({ I }) => {
-
+Before(({ I }) => {
+  I.amOnPage('/#/like');
 });
+
+Scenario('showing empty liked movies', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+})
