4ef9d3355459a78cd4b2e5b522ce848d3a859044
ryfazrin <riansera515@gmail.com>
2022-12-04 15:43:41 +0700

    added: new test

--- specs/favoriteMovieShowSpec.js
+++ specs/favoriteMovieShowSpec.js
@@ -1,5 +1,12 @@
 describe('Showing all favorite movies', () => {
   describe('When no movies have been liked', () => {
+    it('should render the information that no movies have been liked', () => {
+      const presenter = new FavoriteMovieShowPresenter({
+        view,
+      });
 
+      const movies = [];
+      presenter._displayMovies(movies);
+    });
   });
 });
