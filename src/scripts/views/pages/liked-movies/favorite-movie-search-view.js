ad6009da103a74b78e674a343ad81ebb60133c1c
ryfazrin <riansera515@gmail.com>
2022-12-04 17:18:57 +0700

    updated: abstract getTemplate

--- src/scripts/views/pages/liked-movies/favorite-movie-search-view.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-view.js
@@ -3,13 +3,14 @@ import { createMovieItemTemplate } from "../../templates/template-creator";
 class FavoriteMovieSearchView {
   getTemplate() {
     return `
+    <div class="content">
+      <input type="text" id="query">
+      <h2 class="content__heading">Your Liked Movie</h2>
       <div id="movie-search-container">
-        <input id="query" type="text">
-        <div class="movie-result-container">
-          <ul class="movies">
-          </ul>
+        <div id="movies" class="movies">
         </div>
       </div>
+    </div>
     `;
   }
 
