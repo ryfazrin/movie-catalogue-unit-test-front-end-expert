84937d4d195066b1c66fd9c1454e8b12fa8a5ac6
ryfazrin <riansera515@gmail.com>
2022-12-04 15:48:53 +0700

    created: FavoriteMovieShowPresenter and use this

--- specs/favoriteMovieShowSpec.js
+++ specs/favoriteMovieShowSpec.js
@@ -1,4 +1,17 @@
+import FavoriteMovieShowPresenter from '../src/scripts/views/pages/liked-movies/favorite-movie-show-presenter';
+
 describe('Showing all favorite movies', () => {
+  let view;
+
+  const rendertemplate = () => {
+    view = new FavoriteMovieSearchView();
+    document.body.innerHTML = view.getFavoriteMovieTemplate();
+  };
+
+  beforeEach(() => {
+    rendertemplate();
+  });
+
   describe('When no movies have been liked', () => {
     it('should render the information that no movies have been liked', () => {
       const presenter = new FavoriteMovieShowPresenter({
@@ -7,6 +20,9 @@ describe('Showing all favorite movies', () => {
 
       const movies = [];
       presenter._displayMovies(movies);
+
+      expect(document.querySelectorAll('.movie-item__not__found').length)
+        .toEqual(1);
     });
   });
 });
