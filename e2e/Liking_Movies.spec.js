f915856603fe93a14073d1205abc2f3491d685b7
ryfazrin <riansera515@gmail.com>
2022-12-04 19:38:24 +0700

    added: new scenario liking one movie

--- e2e/Liking_Movies.spec.js
+++ e2e/Liking_Movies.spec.js
@@ -7,4 +7,10 @@ Before(({ I }) => {
 Scenario('showing empty liked movies', ({ I }) => {
   I.seeElement('#query');
   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
-})
+});
+
+Scenario('liking one movie', ({ I }) => {
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+
+  I.amOnPage('/');
+});
