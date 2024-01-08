88ffd56a362abefdcec510ee5bd9a7673c7a0fa4
ryfazrin <riansera515@gmail.com>
2024-01-08 10:44:41 +0800

    Initialize movie catalogue automated testing implemented

--- e2e/Liking_Movies.spec.js
+++ e2e/Liking_Movies.spec.js
@@ -6,10 +6,10 @@ Before(({ I }) => {
   I.amOnPage('/#/like');
 });
 
-// Scenario('showing empty liked movies', ({ I }) => {
-//   I.seeElement('#query');
-//   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
-// });
+Scenario('showing empty liked movies', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+});
 
 // Scenario('liking one movie', async ({ I }) => {
 //   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
